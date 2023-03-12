027fbe0565a93fef1bf18e20072cc36dc9127a63
DuXico <chico_emanuelsoares@hotmail.com>
2023-03-12 19:04:02 -0300

    gallery sem scroll

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -1,26 +1,10 @@
 import Image from 'next/image';
-import { useRef } from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import SubHeading from '../SubHeading/SubHeading';
 import images from '../images';
 
-type direction = string
 
 const Gallery = () => {
-
-  const scrollRef = useRef(null);
-  const current = scrollRef;
-
-  const scroll = (props:direction) => {
-    const direction = props
-    if (direction === 'left') {
-      current.scrollLeft -= 300;
-    } else {
-      current.scrollLeft += 300;
-    }
-  };
-
-  
   return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
@@ -30,7 +14,7 @@ const Gallery = () => {
         <button type="button" className="custom__button">View More</button>
       </div>
       <div className="app__gallery-images">
-        <div className="app__gallery-images_container" ref={scrollRef}>
+        <div className="app__gallery-images_container">
           {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
             <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
               <Image src={image} alt="gallery_image" />
@@ -39,8 +23,8 @@ const Gallery = () => {
           ))}
         </div>
         <div className="app__gallery-images_arrows">
-          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
-          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+          <BsArrowLeftShort className="gallery__arrow-icon" />
+          <BsArrowRightShort className="gallery__arrow-icon" />
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Navbar />
       <Header />
       <AboutUs />
